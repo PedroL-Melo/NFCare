dcfee15a46c5a9bb1ad00b8a6b4a7dcafbd7e585
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-09-21 23:41:30 -0500

    Fix: Trust proxies to solve Mixed Content CSS bug on Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,7 @@ $app = Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (\Throwable $e) {
